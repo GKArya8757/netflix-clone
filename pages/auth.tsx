39b4c0304a638714607db63a93c650fcc6ffcbf9
Gurudatt Kumar <gurudatt.kumar@solugenix.com>
2023-05-01 12:16:02 +0530

    Netflix-Clone App

---
+++ pages/auth.tsx
@@ -0,0 +1,123 @@
+import axios from "axios";
+import Input from "@/components/Input";
+import { useCallback, useState } from "react";
+import { signIn } from "next-auth/react";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [variant, setvariant] = useState("login");
+  const toggleVariant = useCallback(() => {
+    setvariant((currentVariant) =>
+      currentVariant === "login" ? "register" : "login"
+    );
+  }, []);
+
+  const login = useCallback(async () => {
+    try {
+      await signIn("credentials", {
+        email,
+        password,
+        callbackUrl: "/profiles",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, password]);
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post("/api/register", {
+        email,
+        name,
+        password,
+      });
+      login();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, name, password, login]);
+
+  return (
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-cover bg-fixed bg-center bg-no-repeat">
+      <div className="h-full w-full bg-black lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img src="/images/logo.png" alt="logo" className="h-12" />
+        </nav>
+        <div className="flex justify-center">
+          <div className="mt-2 w-full self-center rounded-md bg-black bg-opacity-70 px-16 py-16 lg:w-2/5 lg:max-w-md">
+            <h2 className="mb-8 text-4xl font-semibold text-white">
+              {variant === "login" ? "Sign in" : "Register"}
+            </h2>
+            <div className="flex flex-col gap-4">
+              {variant === "register" && (
+                <Input
+                  label="Username"
+                  onChange={(e: any) => setName(e.target.value)}
+                  id="name"
+                  type="text"
+                  value={name}
+                />
+              )}
+              <Input
+                label="Email"
+                onChange={(e: any) => setEmail(e.target.value)}
+                id="email"
+                type="email"
+                value={email}
+              />
+              <Input
+                label="Password"
+                onChange={(e: any) => setPassword(e.target.value)}
+                id="password"
+                type="password"
+                value={password}
+              />
+            </div>
+            <button
+              onClick={variant === "login" ? login : register}
+              className="mt-10 w-full rounded-md bg-red-600 py-3 text-white transition hover:bg-red-700"
+            >
+              {variant === "login" ? "Login" : "Sign up"}
+            </button>
+            <div className="mt-8 flex flex-row items-center justify-center gap-4">
+              <div
+                onClick={(e) => {
+                  e.preventDefault();
+                  signIn("google", { callbackUrl: "/profiles" });
+                }}
+                className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-white transition hover:opacity-80"
+              >
+                <FcGoogle size={30} />
+              </div>
+              <div
+                onClick={() => {
+                  signIn("github", { callbackUrl: "/profiles" });
+                }}
+                className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-white transition hover:opacity-80"
+              >
+                <FaGithub size={30} />
+              </div>
+            </div>
+            <p className="mt-12 text-neutral-500">
+              {variant === "login"
+                ? "First time using Netflix?"
+                : "Already have an account?"}
+              <span
+                onClick={toggleVariant}
+                className="ml-1 cursor-pointer text-white hover:underline"
+              >
+                {variant === "login" ? "Create and account" : "Login"}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
